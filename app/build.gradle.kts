e8cab31a7c2c9dfbfa40d42c11814196e856f6b5
aditya29mishra <adityarakeshmishr@gmail.com>
2025-02-05 04:12:28 +0530

    updated dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,6 +73,8 @@ dependencies {
     implementation (libs.androidx.room.runtime)
     implementation (libs.androidx.room.ktx)
     ksp (libs.androidx.room.compiler)
+    implementation (libs.androidx.work.runtime.ktx)
+
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
