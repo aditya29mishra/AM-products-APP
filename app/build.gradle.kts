67ec350e02467e0e4306d93d5357c50498d3397b
aditya29mishra <adityarakeshmishr@gmail.com>
2025-02-04 21:59:47 +0530

    Added Room Gradle dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,9 @@ dependencies {
     implementation (libs.androidx.navigation.compose)
 
 
+    implementation (libs.androidx.room.runtime)
+    implementation (libs.androidx.room.ktx)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
